ff6694a3769e2c43258794ee12d0329aa68eda36
Morgan Harper <23HarperM@mydacc.org>
2023-02-13 12:54:57 -0500

    Added html pages and some content.
    Co-authored-by: Spencer <skaongit@users.noreply.github.com>

---
+++ javascript/scripts.js
@@ -0,0 +1,43 @@
+//Scroll back to top
+var mybutton = document.getElementById("myBtn");
+    function scrollFunction2() {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+    //Event listeners for both navigation and top button
+    window.addEventListener('scroll', navFunction);
+    window.addEventListener('scroll', scrollFunction2);
+
+
+
+
+//Mobile navigation
+function openNav() {
+    document.getElementById("m-ulnav").style.height = "550px";
+    document.getElementById("m-ulnav").style.marginBottom = "260px";
+    document.getElementById("m-ulnav").style.border = "5px solid #d3d3d3";
+    document.getElementById("m-ulnav").style.marginLeft = "-4px";
+
+    document.getElementById("openNav").style.display = "none";
+
+    document.getElementById("closeNav").style.display = "block";
+  }
+
+  function closeNav() {
+    document.getElementById("m-ulnav").style.height = "60px";
+    document.getElementById("m-ulnav").style.marginBottom = "0px";
+    document.getElementById("m-ulnav").style.border = "1px solid #d3d3d3";
+    document.getElementById("m-ulnav").style.marginLeft = "0px";
+
+    document.getElementById("closeNav").style.display = "none";
+
+    document.getElementById("openNav").style.display = "block";
+  }
+
