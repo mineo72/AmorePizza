11fffbbdadebefbe8632f4cd0baba499f7f1d0c7
Morgan Harper <23HarperM@mydacc.org>
2023-02-21 12:35:45 -0500

    Added code from weekend work.
    Co-authored-by: Spencer <skaongit@users.noreply.github.com>

---
+++ javascript/settings.js
@@ -0,0 +1,15 @@
+function hideAll() {
+    document.getElementById("settingHome").style.display = "none";
+    document.getElementById("settingAccount").style.display = "none";
+    document.getElementById("settingStyle").style.display = "none";
+}
+
+function clickHome() {
+    document.getElementById("settingHome").style.display = "block";
+}
+function clickAccount() {
+    document.getElementById("settingAccount").style.display = "block";
+}
+function clickStyle() {
+    document.getElementById("settingStyle").style.display = "block";
+}
